073e96ee8c524bd9270e2a8f5cfbe9aabd58eb64
Jean <jean.m@thjn.fr>
2022-04-07 12:23:46 +0200

    correction ret ligne

--- www_data/index.php
+++ www_data/index.php
@@ -146,7 +146,7 @@ $methode = [1 => "temp", 2 => "glaire", 3 => "fc"];
 				<br class="pas_glaire pas_fc" />
 				<span class="categorie pas_fc pas_glaire">Température:</span><br class="pas_glaire" />
 				<input class="pas_fc pas_glaire" type="number" step="0.1" min="0" max="99" name="temp" id="form_temp" style="width: 100px;" placeholder="ex: 36,5" /><span class="pas_glaire pas_fc"> °C 🌡️</span><br class="pas_glaire pas_fc"/>
-				<br class="pas_fc pas_glaire" />
+				<br class="pas_fc " />
 				<span class="categorie pas_fc">Vos sensations et visuels:</span><br class="pas_fc" />
 				<span class="pas_fc" id="vos_obs"></span>
 				<input class="pas_fc" type="text" name="ob_extra" id="ob_extra" autocapitalize="off" style="width: 95%" placeholder="autres sensations/visuels (séparées par une virgule)"/><br class="pas_fc" />
