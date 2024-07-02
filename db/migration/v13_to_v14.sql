57680941cc9afbcee47531f1fab9985628e0b6c9
Jean <jean.m@thjn.fr>
2024-07-02 08:06:12 +0200

    DB duplicate cleaning

--- db/migration/v13_to_v14.sql
+++ db/migration/v13_to_v14.sql
@@ -1,1 +1,13 @@
+delete a from observation a join (select no_observation from observation group by date_obs, no_compte having count(no_observation)>1) b on a.no_observation = b.no_observation;
+delete a from observation a join (select no_observation from observation group by date_obs, no_compte having count(no_observation)>1) b on a.no_observation = b.no_observation;
+delete a from observation a join (select no_observation from observation group by date_obs, no_compte having count(no_observation)>1) b on a.no_observation = b.no_observation;
+delete a from observation a join (select no_observation from observation group by date_obs, no_compte having count(no_observation)>1) b on a.no_observation = b.no_observation;
+delete a from observation a join (select no_observation from observation group by date_obs, no_compte having count(no_observation)>1) b on a.no_observation = b.no_observation;
+delete a from observation a join (select no_observation from observation group by date_obs, no_compte having count(no_observation)>1) b on a.no_observation = b.no_observation;
+delete a from observation a join (select no_observation from observation group by date_obs, no_compte having count(no_observation)>1) b on a.no_observation = b.no_observation;
+delete a from observation a join (select no_observation from observation group by date_obs, no_compte having count(no_observation)>1) b on a.no_observation = b.no_observation;
+delete a from observation a join (select no_observation from observation group by date_obs, no_compte having count(no_observation)>1) b on a.no_observation = b.no_observation;
+delete a from observation a join (select no_observation from observation group by date_obs, no_compte having count(no_observation)>1) b on a.no_observation = b.no_observation;
+delete a from observation a join (select no_observation from observation group by date_obs, no_compte having count(no_observation)>1) b on a.no_observation = b.no_observation;
+
 alter table observation add constraint unique_compte_and_date unique (no_compte, date_obs);
